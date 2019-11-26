511248e643d0d587c13c950bc1d9fd98bb1e9228
mfis <fischermatthias1980@googlemail.com>
2019-11-26 20:56:28 +0100

    Command UnitTests

---
+++ Home/homelibrary/src/test/java/homelibrary/homematic/model/HomematicCommandTest.java
@@ -0,0 +1,69 @@
+package homelibrary.homematic.model;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class HomematicCommandTest {
+
+	@Test
+	public void testBuildCommandReadDeviceDatapoint() throws Exception {
+		assertThat(HomematicCommand.read(Device.STROMZAEHLER, Datapoint.POWER).buildCommand(), is(
+				"var VAR_BidCos_RF_NEQ0861520_1_POWER = (datapoints.Get('BidCos-RF.NEQ0861520:1.POWER')).Value();"));
+	}
+
+	@Test
+	public void testBuildCommandReadDeviceSuffix() throws Exception {
+		assertThat(HomematicCommand.read(Device.STROMZAEHLER, "SUFFIX").buildCommand(),
+				is("var VAR_StromverbrauchHausSUFFIX = dom.GetObject('StromverbrauchHausSUFFIX').State();"));
+	}
+
+	@Test
+	public void testBuildCommandReadName() throws Exception {
+		assertThat(HomematicCommand.read("NAME").buildCommand(),
+				is("var VAR_NAME = dom.GetObject('NAME').State();"));
+	}
+
+	@Test
+	public void testBuildCommandReadTSDeviceDatapoint() throws Exception {
+		assertThat(HomematicCommand.readTS(Device.STROMZAEHLER, Datapoint.POWER).buildCommand(), is(
+				"var VAR_BidCos_RF_NEQ0861520_1_POWER_TS = (datapoints.Get('BidCos-RF.NEQ0861520:1.POWER')).LastTimestamp();"));
+	}
+
+	@Test
+	public void testBuildCommandWriteDeviceDatapointBoolean() throws Exception {
+		assertThat(HomematicCommand.write(Device.STROMZAEHLER, Datapoint.POWER, true).buildCommand(), is(
+				"var RC_BidCos_RF_NEQ0861520_1_POWER = (datapoints.Get('BidCos-RF.NEQ0861520:1.POWER')).State(true);"));
+	}
+
+	@Test
+	public void testBuildCommandWriteDeviceDatapointString() throws Exception {
+		assertThat(HomematicCommand.write(Device.STROMZAEHLER, Datapoint.POWER, "NEWVALUE").buildCommand(),
+				is("var RC_BidCos_RF_NEQ0861520_1_POWER = (datapoints.Get('BidCos-RF.NEQ0861520:1.POWER')).State('NEWVALUE');"));
+	}
+
+	@Test
+	public void testBuildCommandWriteDeviceSuffixBoolean() throws Exception {
+		assertThat(HomematicCommand.write(Device.STROMZAEHLER, "SUFFIX", true).buildCommand(), is(
+				"var RC_StromverbrauchHausSUFFIX = dom.GetObject('StromverbrauchHausSUFFIX').State(true);"));
+	}
+
+	@Test
+	public void testBuildCommandWriteDeviceSuffixString() throws Exception {
+		assertThat(HomematicCommand.write(Device.STROMZAEHLER, "SUFFIX", "NEWVALUE").buildCommand(), is(
+				"var RC_StromverbrauchHausSUFFIX = dom.GetObject('StromverbrauchHausSUFFIX').State('NEWVALUE');"));
+	}
+
+	@Test
+	public void testBuildCommandWriteNameString() throws Exception {
+		assertThat(HomematicCommand.write("NAME", "NEWVALUE").buildCommand(),
+				is("var RC_NAME = dom.GetObject('NAME').State('NEWVALUE');"));
+	}
+
+	@Test
+	public void testBuildCommandExecDeviceSuffix() throws Exception {
+		assertThat(HomematicCommand.exec(Device.STROMZAEHLER, "SUFFIX").buildCommand(), is(
+				"var RC_StromverbrauchHausSUFFIX = dom.GetObject('StromverbrauchHausSUFFIX').ProgramExecute();"));
+	}
+}
