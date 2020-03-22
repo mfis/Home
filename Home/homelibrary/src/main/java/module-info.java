7b91521fc0d077dc416a14a40a51a05946ec5d97
mfis <fischermatthias1980@googlemail.com>
2020-03-22 12:29:41 +0100

    deleted module-info files

--- Home/homelibrary/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module homelibrary {
-	exports de.fimatas.home.library.dao;
-	exports de.fimatas.home.library.model;
-	exports de.fimatas.home.library.util;
-	exports de.fimatas.home.library.domain.model;
-	exports de.fimatas.home.library.homematic.model;
-	
-}
