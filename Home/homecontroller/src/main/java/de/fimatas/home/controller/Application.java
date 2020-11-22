c6c07922233a0ece3e856e0eab78d05e0aac9796
mfis <fischermatthias1980@googlemail.com>
2020-11-22 13:51:30 +0100

    upload state error handling

--- Home/homecontroller/src/main/java/de/fimatas/home/controller/Application.java
+++ Home/homecontroller/src/main/java/de/fimatas/home/controller/Application.java
@@ -2,11 +2,14 @@ package de.fimatas.home.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@ComponentScan(basePackages = {"de.fimatas.home", "mfi.files"})
+
 @PropertySource(value = "classpath:application.properties", encoding = "UTF-8")
 @PropertySource(value = "file:/Users/mfi/documents/config/homecontroller.properties", encoding = "UTF-8", ignoreResourceNotFound = true)
 @PropertySource(value = "file:/home/homecontroller/homecontroller.properties", encoding = "UTF-8", ignoreResourceNotFound = true)
--- Home/homecontroller/src/main/java/de/fimatas/home/controller/domain/service/UploadService.java
+++ Home/homecontroller/src/main/java/de/fimatas/home/controller/domain/service/UploadService.java
@@ -1,8 +1,8 @@
 package de.fimatas.home.controller.domain.service;
 
 import java.util.Arrays;
-
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -12,8 +12,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-
 import de.fimatas.home.library.util.HomeAppConstants;
 
 @Component
@@ -24,6 +26,10 @@ public class UploadService {
 
     @Autowired
     private Environment env;
+
+    private static Log log = LogFactory.getLog(UploadService.class);
+
+    private long resourceNotAvailableCounter;
 
     public void upload(Object object) {
         String host = env.getProperty("client.hostName");
@@ -48,15 +54,32 @@ public class UploadService {
             HttpEntity<T> request = new HttpEntity<>((T) instance, headers);
             ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
             HttpStatus statusCode = response.getStatusCode();
+
+            connectionEstablishedLogging();
             if (!statusCode.is2xxSuccessful()) {
                 LogFactory.getLog(UploadService.class).error("Could not successful upload data. RC=" + statusCode.value());
             }
 
-        } catch (Exception e) {
-            LogFactory.getLog(UploadService.class)
-                .warn("Could not upload data:" + instance.getClass().getSimpleName() + ":" + e.getMessage());
+        } catch (ResourceAccessException | HttpServerErrorException e) {
+            connectionNotEstablishedLogging(e);
         }
         return t;
+    }
+
+    private void connectionNotEstablishedLogging(RestClientException e) {
+        resourceNotAvailableCounter++;
+        String suppressLogEntries = resourceNotAvailableCounter == 5 ? " NO FURTHER LOG ENTRIES WILL BE WRITTEN." : "";
+        if (resourceNotAvailableCounter < 4) {
+            log.warn("Could not upload state (#" + resourceNotAvailableCounter + "). "
+                + e.getMessage().replace('\r', ' ').replace('\n', ' ') + suppressLogEntries);
+        }
+    }
+
+    private void connectionEstablishedLogging() {
+        if (resourceNotAvailableCounter > 0) {
+            log.warn("upload state successful after " + resourceNotAvailableCounter + " times.");
+            resourceNotAvailableCounter = 0;
+        }
     }
 
 }
