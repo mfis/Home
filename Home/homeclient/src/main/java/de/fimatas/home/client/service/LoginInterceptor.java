1c8c51ffebddf8c751263bf941b5f8918ba46ef6
mfis <fischermatthias1980@googlemail.com>
2022-05-07 20:00:02 +0200

    opt-out for redirect after login failure

--- Home/homeclient/src/main/java/de/fimatas/home/client/service/LoginInterceptor.java
+++ Home/homeclient/src/main/java/de/fimatas/home/client/service/LoginInterceptor.java
@@ -254,7 +254,11 @@ public class LoginInterceptor implements HandlerInterceptor {
                 return userService.userNameFromLoginCookie(token);
             }
         } else {
-            response.sendRedirect(LoginController.LOGIN_FAILED_URI);
+            if(BooleanUtils.toBoolean(request.getHeader("noRedirect"))){
+                response.setStatus(HttpStatus.UNAUTHORIZED.value()); // 401
+            }else{
+                response.sendRedirect(LoginController.LOGIN_FAILED_URI);
+            }
             logoff(request, response);
             return null;
         }
