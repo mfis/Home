af92edd9760ae27875de59a8de3c98350509367a
mfis <fischermatthias1980@googlemail.com>
2022-09-18 14:20:33 +0200

    separate application properties into credentials an others

--- Home/homecontroller/src/main/java/de/fimatas/home/controller/HomeControllerApplication.java
+++ Home/homecontroller/src/main/java/de/fimatas/home/controller/HomeControllerApplication.java
@@ -16,6 +16,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @PropertySource(value = "classpath:application.properties", encoding = "UTF-8")
 @PropertySource(value = "file:/Users/mfi/documents/config/homecontroller.properties", encoding = "UTF-8", ignoreResourceNotFound = true)
 @PropertySource(value = "file:/home/homecontroller/homecontroller.properties", encoding = "UTF-8", ignoreResourceNotFound = true)
+@PropertySource(value = "file:/Users/mfi/documents/config/homecontrollercredentials.properties", encoding = "UTF-8", ignoreResourceNotFound = true)
+@PropertySource(value = "file:/home/homecontroller/homecontrollercredentials.properties", encoding = "UTF-8", ignoreResourceNotFound = true)
 public class HomeControllerApplication { // NOSONAR
     public static void main(String[] args) {
         SpringApplication.run(HomeControllerApplication.class, args);
