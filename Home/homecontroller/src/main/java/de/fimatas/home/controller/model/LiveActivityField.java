7f863579a331f562c9f833bc51231ad5fb02194c
mfis <fischermatthias1980@googlemail.com>
2023-08-09 22:12:42 +0200

    LiveActivityField format

--- Home/homecontroller/src/main/java/de/fimatas/home/controller/model/LiveActivityField.java
+++ Home/homecontroller/src/main/java/de/fimatas/home/controller/model/LiveActivityField.java
@@ -3,16 +3,26 @@ package de.fimatas.home.controller.model;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import java.util.function.Function;
 
 @Getter
 public enum LiveActivityField {
 
-    ELECTRIC_GRID(new BigDecimal(50)), //
+    ELECTRIC_GRID(new BigDecimal(50), val -> "format_1_" + val.toString()), //
+    EV_CHARGE(new BigDecimal(1), val -> "format_2_" + val.toString()), //
     ;
 
     private final BigDecimal thresholdMin;
 
-    LiveActivityField(BigDecimal thresholdMin){
+    private final Function<BigDecimal, String> formatter;
+
+    LiveActivityField(BigDecimal thresholdMin, Function<BigDecimal, String> formatter){
         this.thresholdMin = thresholdMin;
+        this.formatter = formatter;
     }
+
+    public String format(BigDecimal value){
+        return formatter.apply(value);
+    }
+
 }
