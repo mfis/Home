d2de779641a264d8954dd8f5a262976fabadeb34
mfis <fischermatthias1980@googlemail.com>
2024-01-17 21:33:55 +0100

    edited charge limits

--- Home/homelibrary/src/main/java/de/fimatas/home/library/domain/model/ChargeLimit.java
+++ Home/homelibrary/src/main/java/de/fimatas/home/library/domain/model/ChargeLimit.java
@@ -1,10 +1,14 @@
 package de.fimatas.home.library.domain.model;
 
+import lombok.Getter;
+
+@Getter
 public enum ChargeLimit {
 
+    _60((short)60, "60%"), //
     _70((short)70, "70%"), //
     _80((short)80, "80%"), //
-    _85((short)85, "85%"), //
+    _90((short)90, "90%"), //
     MAX((short)100, "Max"), //
     ;
 
@@ -12,17 +16,10 @@ public enum ChargeLimit {
 
     private final String caption;
 
-    private ChargeLimit(short percentage, String caption) {
+    ChargeLimit(short percentage, String caption) {
         this.percentage = percentage;
         this.caption = caption;
         if(this.name().length()>8) throw new IllegalArgumentException("name too long: " + this.name());
     }
 
-    public short getPercentage() {
-        return percentage;
-    }
-
-    public String getCaption() {
-        return caption;
-    }
 }
