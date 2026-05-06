e5420b8d6157b1c646d41bb89583f0bb10158eab
mfis <fischermatthias1980@googlemail.com>
2026-05-06 19:43:33 +0200

    version logging

--- Home/homecontroller/src/main/java/de/fimatas/home/controller/HomeControllerApplication.java
+++ Home/homecontroller/src/main/java/de/fimatas/home/controller/HomeControllerApplication.java
@@ -1,7 +1,11 @@
 package de.fimatas.home.controller;
 
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
@@ -13,7 +17,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAspectJAutoProxy
 @SpringBootApplication
 @ComponentScan(basePackages = {"de.fimatas.home"})
-
+@CommonsLog
 @PropertySource(value = "classpath:application.properties", encoding = "UTF-8")
 @PropertySource(value = "file:/Users/mfi/documents/config/homecontroller/homecontroller.properties", encoding = "UTF-8", ignoreResourceNotFound = true)
 @PropertySource(value = "file:/opt/homecontroller/homecontroller.properties", encoding = "UTF-8", ignoreResourceNotFound = true)
@@ -21,6 +25,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @PropertySource(value = "file:/opt/homecontroller/homecontrollercredentials.properties", encoding = "UTF-8", ignoreResourceNotFound = true)
 public class HomeControllerApplication { // NOSONAR
     public static void main(String[] args) {
-        SpringApplication.run(HomeControllerApplication.class, args);
+        SpringApplication app = new SpringApplication(HomeControllerApplication.class);
+        app.addListeners((ApplicationListener<ApplicationStartedEvent>) event -> {
+            log.info("Spring Boot Version: " + SpringBootVersion.getVersion());
+        });
+        app.run(args);
     }
 }
